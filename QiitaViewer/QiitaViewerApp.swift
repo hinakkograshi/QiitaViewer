9a7031f71eab2b4ca3a27081ff9d8a2336bf9e56
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-08-18 21:10:02 +0900

    add: First Commit

---
+++ QiitaViewer/QiitaViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QiitaViewerApp.swift
+//  QiitaViewer
+//
+//  Created by Hina on 2024/08/18.
+//
+
+import SwiftUI
+
+@main
+struct QiitaViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
