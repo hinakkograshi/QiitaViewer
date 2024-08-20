e52c0d297618f75b7e6cae5965cb596090614c65
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-08-20 22:59:32 +0900

    delete: CustomAlert削除

--- QiitaViewer/ViewModels/QiitaSearchViewModel.swift
+++ QiitaViewer/ViewModels/QiitaSearchViewModel.swift
@@ -11,55 +11,27 @@ import Foundation
 final class QiitaSearchViewModel: ObservableObject {
     @Published var articles = [Article]()
     @Published var isPresentingAlert = false
-    @Published var currentAlertType: CustomAlert.AlertType?
     @Published var loadingState: LoadingState = .idle
     @Published var searchText: String = ""
 
-
-    func fetchAllArticles() async throws {
+    func fetchAllArticles(query: String) async throws {
         do {
             loadingState = .loading
-            articles = try await ArticleAPIClient().fetchArticles(query: searchText)
+            articles = try await ArticleAPIClient().fetchArticles(query: query)
             loadingState = .success
         } catch let error as ArticleAPIClientError {
-            loadingState = .failure
             switch error {
             case .invalidURL:
-                await showNetWorkErrorAlert()
+                loadingState = .networkFailure
             case .unexpectedResponse:
-                await showRetryAlert()
+                loadingState = .networkFailure
             case .networkError:
-                await showRetryAlert()
+                loadingState = .networkFailure
             case .serverError:
-                showServerErrorAlert()
+                loadingState = .serverFailure
             }
         } catch {
-            loadingState = .failure
-            await showRetryAlert()
+            loadingState = .networkFailure
         }
-    }
-
-    private func showNetWorkErrorAlert() async {
-        closeAlert(title: "通信エラー", message: "通信することができませんでした。")
-    }
-
-    private func showServerErrorAlert() {
-        closeAlert(title: "メンテナンス中", message: "メンテナンス後にお試しください。")
-    }
-
-    private func showRetryAlert() async {
-        await retryAlert(title: "通信エラー", message: "もう一度通信しますか？")
-    }
-
-    private func closeAlert(title: String, message: String) {
-        currentAlertType = .close(title: title, message: message)
-        isPresentingAlert = true
-    }
-
-    private func retryAlert(title: String, message: String) async {
-        currentAlertType = .retry(title: title, message: message) { [weak self] in
-            try? await self?.fetchAllArticles()
-        }
-        isPresentingAlert = true
     }
 }
